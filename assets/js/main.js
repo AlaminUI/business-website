72d968078b5f53e064c729717e396e30a110f19f
AlaminUI <alaminislam907@gmail.com>
2023-07-12 09:58:02 +0600

    upate

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    ma5menu({
+            menu: '.site-menu',
+            activeClass: 'active',
+            footer: '#ma5menu-tools',
+            position: 'left',
+            closeOnBodyClick: true
+          });
+  })
